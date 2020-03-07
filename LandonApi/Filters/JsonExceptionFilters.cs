82f8280e3c7e509a9730889546d5ffc9b7c8e2f3
Bimal Varghese <bimalvv2005@gmail.com>
2020-03-07 17:17:08 +0530

    Add project files.

---
+++ LandonApi/Filters/JsonExceptionFilters.cs
@@ -0,0 +1,44 @@
+﻿using LandonApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Filters
+{
+    public class JsonExceptionFilters : IExceptionFilter
+    {
+        public JsonExceptionFilters(IHostingEnvironment env)
+        {
+            this._env = env;
+        }
+
+        private readonly IHostingEnvironment _env;
+
+        public void OnException(ExceptionContext context)
+        {
+            var error = new ApiError();
+            if (this._env.IsDevelopment())
+            {
+
+                error.Message = context.Exception.Message;
+                error.Details = context.Exception.StackTrace;
+            }
+            else
+            {
+                error.Message = "A server error occured";
+                error.Details = context.Exception.Message;
+            }
+
+            context.Result = new ObjectResult(error)
+            {
+                StatusCode = 500
+
+            };
+        }
+    }
+}
---
+++ LandonApi/Models/ApiError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Models
+{
+    public class ApiError
+    {
+        public string Message{ get; set; }
+        public string Details { get; set; }
+    }
+}
