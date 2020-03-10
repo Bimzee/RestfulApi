22c6d81a0f8b54d88b95f6c9f1cb7a3fe3ae8714
Bimal Varghese <bimalvv2005@gmail.com>
2020-03-10 11:49:33 +0530

    Successfully returned data from DB and returned it in ION format

--- LandonApi/Controllers/RoomsController.cs
+++ LandonApi/Controllers/RoomsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LandonApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,35 @@ namespace LandonApi.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        private readonly HotelApiDbContext _context;
+
+        public RoomsController(HotelApiDbContext context)
+        {
+            this._context = context;
+        }
         [HttpGet(Name = nameof(GetRooms))]
         public IActionResult GetRooms()
         {
             throw new NotImplementedException();
+        }
+
+        [HttpGet("{roomId}",Name =nameof(GetRoomById))]
+        public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+        {
+            var entity = await _context.Rooms.SingleOrDefaultAsync(x => x.Id == roomId);
+
+            if(entity == null)
+            {
+                return NotFound();
+            }
+            var resource = new Room
+            {
+                Href = Url.Link(nameof(GetRoomById), new { roomId = entity.Id }),
+                Name = entity.Name,
+                Rate = entity.Rate / 100.0m
+            };
+
+            return resource;
         }
     }
 }
